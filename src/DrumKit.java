68c343e82e75da5901ab740b51e55c84ec84a0cd
finico <finico.a@gmail.com>
2014-10-02 13:00:27 +0400

    (magnets) DrumKit 73;

---
+++ src/DrumKit.java
@@ -0,0 +1,29 @@
+class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("бах бах ба-бах");
+    }
+
+    void playTopHat() {
+        System.out.println("динь динь ди-динь");
+    }
+}
+
+
+class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+
+        d.playSnare();
+
+        d.snare = false;
+
+        if (d.snare) {
+            d.playSnare();
+        }
+
+        d.playTopHat();
+    }
+}
